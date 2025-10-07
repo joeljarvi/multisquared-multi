3a0595153314b35dc66a5a1f4db82be089a00844
joeljarvi <joeljarvi@live.se>
2025-10-07 15:01:22 +0200

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
 import CasesDisplay from "@/components/cases-display";
-import AuthButton from "@/components/auth-button";
-import { EnvVarWarning } from "@/components/env-var-warning";
-import { ConnectSupabaseSteps } from "@/components/tutorial/connect-supabase-steps";
-import { SignUpUserSteps } from "@/components/tutorial/sign-up-user-steps";
-import { hasEnvVars } from "@/lib/utils";
+
 import { fetchAllCases } from "@/lib/supabase/server";
 
 export default async function Home() {
@@ -11,9 +7,6 @@ export default async function Home() {
 
   return (
     <main className="min-h-screen flex flex-col items-center gap-8 p-5">
-      {!hasEnvVars ? <EnvVarWarning /> : <AuthButton />}
-      {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
-
       <CasesDisplay cases={cases} />
 
       <footer className="w-full flex items-center justify-center text-center text-xs gap-8 py-16">
